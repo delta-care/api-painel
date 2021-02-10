9aa948872b75de5e17f6605c1ec6f0bbc9dc4d26
David Carvalho <jdavidsilv@gmail.com>
2021-02-10 18:15:02 -0300

    first commit

---
+++ src/main/java/xyz/deltacare/painel/ApiPainelApplication.java
@@ -0,0 +1,13 @@
+package xyz.deltacare.painel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPainelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPainelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/deltacare/painel/ApiPainelApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.deltacare.painel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPainelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
