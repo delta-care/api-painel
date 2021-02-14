84bfd8a50bbd38db87021ecf90abe02df101cc7e
David Carvalho <jdavidsilv@gmail.com>
2021-02-14 14:30:20 -0300

    Adição logger em event

--- src/main/java/xyz/deltacare/painel/controller/PainelEventController.java
+++ src/main/java/xyz/deltacare/painel/controller/PainelEventController.java
@@ -2,25 +2,28 @@ package xyz.deltacare.painel.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Controller;
 import xyz.deltacare.painel.dto.NovoBeneficiarioDto;
-import xyz.deltacare.painel.dto.PainelDto;
 import xyz.deltacare.painel.service.PainelService;
 
 @Controller
 public class PainelEventController {
     private final ObjectMapper objectMapper;
     private final PainelService painelService;
+    private final Logger logger = LoggerFactory.getLogger(PainelEventController.class);
 
     public PainelEventController(ObjectMapper objectMapper, PainelService painelService) {
         this.objectMapper = objectMapper;
         this.painelService = painelService;
     }
 
-    @KafkaListener(topics = "novobeneficiario")
+    @KafkaListener(topics = "beneficiario")
     public void save(final String message, final Acknowledgment acknowledgment) throws JsonProcessingException {
+        logger.info("Lendo mensagem: " + message);
         NovoBeneficiarioDto novoBeneficiarioDto = objectMapper.readValue(message, NovoBeneficiarioDto.class);
         painelService.save(novoBeneficiarioDto);
         acknowledgment.acknowledge();
